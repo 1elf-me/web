164b38618a3074804f1a7dcd939ce5a4f77644ae
1elf <63093594+1elf-me@users.noreply.github.com>
2021-03-23 22:28:28 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <html lang="en">
-	<head>
-		  <meta charset="utf-8">
-		  <title>Example</title>
-      <meta name="description" content="This is a example page.">
-      <meta name="author" content="1elf-me">
+  <head>
+    <meta charset="utf-8">
+		<title>Example</title>
+		<meta name="description" content="This is a example page.">
+		<meta name="author" content="1elf-me">
 	</head>
 	<body>
 		<h1>Example Page</h1>
