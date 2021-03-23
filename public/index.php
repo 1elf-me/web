e8ae8ab0a89103d825e675b7754cbfdf9059ddb3
1elf <63093594+1elf-me@users.noreply.github.com>
2021-03-23 22:27:17 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,12 @@
-<?php
-echo "Git repository!";
+<html lang="en">
+	<head>
+		  <meta charset="utf-8">
+		  <title>Example</title>
+      <meta name="description" content="This is a example page.">
+      <meta name="author" content="1elf-me">
+	</head>
+	<body>
+		<h1>Example Page</h1>
+    <p><?php echo "Hello World!"; ?></p>
+	</body>
+</html>
